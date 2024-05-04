cdd64d7da7f4dce3e8ef250642e7249f222e51ab
devffery <mails4icy@gmail.com>
2024-05-04 04:01:23 +0100

    Implemented Simple Search

--- Store/core/urls.py
+++ Store/core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.frontpage, name='frontpage'),
     path('about/', views.about, name='about'),
+    path('', views.frontpage, name='frontpage'),
 ]
